dee00128d939c40eff3ab69d3d07de222e4a8be0
kjussakov <kjussakov@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2010-07-09 11:43:14 +0000

    Initial structure of the project. Some ideas about the functionality of the modules are expressed as header files

---
+++ common/include/errorHandle.h
@@ -0,0 +1,58 @@
+/*==================================================================================*\
+|                                                                                    |
+|                    EXIP - Efficient XML Interchange Processor                      |
+|                                                                                    |
+|------------------------------------------------------------------------------------|
+| Copyright (c) 2010, EISLAB - Luleå University of Technology                        |
+| All rights reserved.                                                               |
+|                                                                                    |
+| Redistribution and use in source and binary forms, with or without                 |
+| modification, are permitted provided that the following conditions are met:        |
+|     * Redistributions of source code must retain the above copyright               |
+|       notice, this list of conditions and the following disclaimer.                |
+|     * Redistributions in binary form must reproduce the above copyright            |
+|       notice, this list of conditions and the following disclaimer in the          |
+|       documentation and/or other materials provided with the distribution.         |
+|     * Neither the name of the EISLAB - Luleå University of Technology nor the      |
+|       names of its contributors may be used to endorse or promote products         |
+|       derived from this software without specific prior written permission.        |
+|                                                                                    |
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND    |
+| ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED      |
+| WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE             |
+| DISCLAIMED. IN NO EVENT SHALL EISLAB - LULEÅ UNIVERSITY OF TECHNOLOGY BE LIABLE    |
+| FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES |
+| (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;       |
+| LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND        |
+| ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT         |
+| (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      |
+| SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                       |
+|                                                                                    |
+|                                                                                    |
+|                                                                                    |
+\===================================================================================*/
+
+/**
+ * @file errorHandle.h
+ * @brief Error handling codes and function definitions
+ *
+ * @date Jul 7, 2010
+ * @author Rumen Kyusakov
+ * @version 0.1
+ * @par[Revision] $Id$
+ */
+
+#ifndef ERRORHANDLE_H_
+#define ERRORHANDLE_H_
+
+typedef char errorCode;
+
+//TODO: define the rest of the error codes
+
+/* Definitions for error constants. */
+/** No error, everything OK. */
+#define ERR_OK    0
+/** The code for this function is not yet implemented. */
+#define NOT_IMPLEMENTED_YET -127
+
+#endif /* ERRORHANDLE_H_ */
