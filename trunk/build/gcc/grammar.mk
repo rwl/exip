b7b1d638d84d21af5db7f7705281f49074b2475c
kjussakov <kjussakov@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2011-01-28 18:27:41 +0000

    Moving make files and tests. The build now seems to work, although haven't been tested much. It still remain to separate the public header files into separate directory

---
+++ trunk/build/gcc/grammar.mk
@@ -0,0 +1,15 @@
+# StreamIO module build
+
+GRAMMAR_OBJ = $(GRAMMAR_BIN)/eventsEXI.o $(GRAMMAR_BIN)/grammarRules.o $(GRAMMAR_BIN)/grammars.o
+
+$(GRAMMAR_BIN)/lib$(GRAMMAR).a : $(GRAMMAR_OBJ)
+		ar rcs $(GRAMMAR_BIN)/lib$(GRAMMAR).a $(GRAMMAR_OBJ)
+
+$(GRAMMAR_BIN)/eventsEXI.o : $(GRAMMAR_SRC)/include/eventsEXI.h $(GRAMMAR_SRC)/src/eventsEXI.c
+		$(CC) -c $(CFLAGS) $(GRAMMAR_SRC)/src/eventsEXI.c -o $(GRAMMAR_BIN)/eventsEXI.o
+
+$(GRAMMAR_BIN)/grammarRules.o : $(GRAMMAR_SRC)/include/eventsEXI.h $(GRAMMAR_SRC)/include/grammarRules.h $(GRAMMAR_SRC)/src/grammarRules.c
+		$(CC) -c $(CFLAGS) $(GRAMMAR_SRC)/src/grammarRules.c -o $(GRAMMAR_BIN)/grammarRules.o
+		
+$(GRAMMAR_BIN)/grammars.o : $(GRAMMAR_SRC)/include/eventsEXI.h $(GRAMMAR_SRC)/include/grammarRules.h $(GRAMMAR_SRC)/src/grammars.c
+		$(CC) -c $(CFLAGS) $(GRAMMAR_SRC)/src/grammars.c -o $(GRAMMAR_BIN)/grammars.o
