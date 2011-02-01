c9fc093b592aa1296359ece9f07dd9216299b054
kjussakov <kjussakov@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2011-02-01 10:34:43 +0000

    Some implementation work done for the GrammarGen module

---
+++ trunk/src/grammarGen/src/normGrammar.c
@@ -0,0 +1,132 @@
+/*==================================================================================*\
+|                                                                                    |
+|                    EXIP - Efficient XML Interchange Processor                      |
+|                                                                                    |
+|------------------------------------------------------------------------------------|
+| Copyright (c) 2010, EISLAB - Luleå University of Technology                        |
+| All rights reserved.                                                               |
+|                                                                                    |
+| Redistribution and use in source and binary forms, with or without                 |
+| modification, are permitted provided that the following conditions are met:        |
+|     * Redistributions of source code must retain the above copyright               |
+|       notice, this list of conditions and the following disclaimer.                |
+|     * Redistributions in binary form must reproduce the above copyright            |
+|       notice, this list of conditions and the following disclaimer in the          |
+|       documentation and/or other materials provided with the distribution.         |
+|     * Neither the name of the EISLAB - Luleå University of Technology nor the      |
+|       names of its contributors may be used to endorse or promote products         |
+|       derived from this software without specific prior written permission.        |
+|                                                                                    |
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND    |
+| ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED      |
+| WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE             |
+| DISCLAIMED. IN NO EVENT SHALL EISLAB - LULEÅ UNIVERSITY OF TECHNOLOGY BE LIABLE    |
+| FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES |
+| (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;       |
+| LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND        |
+| ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT         |
+| (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      |
+| SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                       |
+|                                                                                    |
+|                                                                                    |
+|                                                                                    |
+\===================================================================================*/
+
+/**
+ * @file normGrammar.c
+ * @brief Implementing the code for normalizing EXI Proto-Grammars
+ * @date Jan 31, 2011
+ * @author Rumen Kyusakov
+ * @version 0.1
+ * @par[Revision] $Id$
+ */
+
+#include "normGrammar.h"
+
+/** Store the information for a single already removed production*/
+static struct removedProdInf
+{
+	unsigned int leftNonTermID;
+	unsigned int rightNonTermID;
+};
+
+errorCode deleteNoTermProductions(EXIStream* strm, struct EXIGrammar** grammar)
+{
+	errorCode tmp_err_code = UNEXPECTED_ERROR;
+	int i = 0;
+	int j = 0;
+
+	int k = 0;
+	int l = 0;
+	int t = 0;
+	unsigned char rmPrFlag = 0;
+	DynArray* removedProdArray;
+	unsigned int rightNonTerm = 0;
+	struct removedProdInf pr;
+	uint32_t elId = 0;
+
+	tmp_err_code = createDynArray(&removedProdArray, sizeof(struct removedProdInf), 5, strm);
+	if(tmp_err_code != ERR_OK)
+		return tmp_err_code;
+
+
+	for(; i < grammar->rulesDimension; i++) // For every rule in the grammar
+	{
+		for(j = 0; j < grammar->ruleArray[i].prodCount; j ++) // For every production in a rule
+		{
+			if(grammar->ruleArray[i].prodArray[j].event.eventType == EVENT_VOID) // If there is no TERM symbol in this production
+			{
+				rightNonTerm = grammar->ruleArray[i].prodArray[j].nonTermID;
+				// Register that we are replacing this production without a terminal symbol
+				pr.leftNonTermID = grammar->ruleArray[i].nonTermID;
+				pr.rightNonTermID = rightNonTerm;
+				tmp_err_code = addDynElement(removedProdArray, &pr, &elID, strm);
+				if(tmp_err_code != ERR_OK)
+					return tmp_err_code;
+
+				for(k = 0; k < grammar->rulesDimension; k++)  // For every rule in the grammar
+				{
+					if(grammar->ruleArray[k].nonTermID == rightNonTerm) // If the Left NON-TERM is eq to the previous Production rightNonTerm
+					{
+						for(l = 0; l < grammar->ruleArray[k].prodCount; l++) // For every production in this second rule
+						{
+							if(!(grammar->ruleArray[k].prodArray[l].event.eventType == EVENT_VOID &&
+									grammar->ruleArray[k].prodArray[l].nonTermID == grammar->ruleArray[i].nonTermID)) // If the right-hand side is not identical to the left-hand side
+							{
+								rmPrFlag = 0;   // Shows if a there is already such production replaced
+								struct removedProdInf* tmpPr;
+								for(t = 0; t < removedProdArray->elementCount; t++)
+								{
+									tmpPr = ((struct removedProdInf* ) removedProdArray->elements)[t];
+									if(grammar->ruleArray[k].prodArray[l].nonTermID == tmpPr->rightNonTermID
+										&& grammar->ruleArray[i].nonTermID == tmpPr->leftNonTermID
+										&& grammar->ruleArray[k].prodArray[l].event.eventType == EVENT_VOID)
+									{
+										rmPrFlag = 1; // This production was already replaced - DO NOT INSERT IT!
+										break;
+									}
+								}
+								if(!rmPrFlag) // Insert this production
+								{
+									if(grammar->ruleArray[i].prodArray[j].event.eventType == EVENT_VOID) // This is the first production inserted - so replace the old one
+									{
+										grammar->ruleArray[i].prodArray[j].event.eventType = grammar->ruleArray[k].prodArray[l].event.eventType;
+										grammar->ruleArray[i].prodArray[j].event.valueType = grammar->ruleArray[k].prodArray[l].event.valueType;
+										grammar->ruleArray[i].prodArray[j].nonTermID = grammar->ruleArray[k].prodArray[l].nonTermID;
+									}
+									else // Second or above production for insertion - insert at the end of Production array
+									{
+										tmp_err_code = addProduction(&(grammar->ruleArray[i]), grammar->ruleArray[k].prodArray[l].code,
+												grammar->ruleArray[k].prodArray[l].event, grammar->ruleArray[k].prodArray[l].nonTermID);
+										if(tmp_err_code != ERR_OK)
+											return tmp_err_code;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
