1aaf1fa17904bf99cba276c71677f41e78714809
guilbard <guilbard@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2011-02-17 07:31:05 +0000

    check_contentio functional under vs2010

--- tests/check_contentIO.c
+++ tests/check_contentIO.c
@@ -128,6 +128,9 @@ int main (void)
 	int number_failed;
 	Suite *s = contentio_suite();
 	SRunner *sr = srunner_create (s);
+#ifdef _MSC_VER
+	srunner_set_fork_status(sr, CK_NOFORK);
+#endif
 	srunner_run_all (sr, CK_NORMAL);
 	number_failed = srunner_ntests_failed (sr);
 	srunner_free (sr);
