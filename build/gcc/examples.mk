35e1b1bd68dca53e0428ab330024af65995eab11
kjussakov <kjussakov@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2012-09-17 17:59:30 +0000

    SEP2 example is removed from the repository

--- build/gcc/examples.mk
+++ build/gcc/examples.mk
@@ -3,6 +3,3 @@ $(EXAMPLES_BIN_DIR)/$(EXAMPLE_1): $(EXAMPLES_SRC_DIR)/simpleDecoding/decodeTestE
 		
 $(EXAMPLES_BIN_DIR)/$(EXAMPLE_2): $(EXAMPLES_SRC_DIR)/simpleEncoding/encodeTestEXI.c
 		$(COMPILE) $(LDFLAGS) $(EXAMPLES_SRC_DIR)/simpleEncoding/encodeTestEXI.c -lexip -o $(EXAMPLES_BIN_DIR)/$(EXAMPLE_2)
-		
-$(EXAMPLES_BIN_DIR)/$(EXAMPLE_3): $(EXAMPLES_SRC_DIR)/SmartEnergy2/sep2.c $(EXAMPLES_SRC_DIR)/SmartEnergy2/sep_2051.c
-		$(COMPILE) $(LDFLAGS) $(EXAMPLES_SRC_DIR)/SmartEnergy2/sep2.c $(EXAMPLES_SRC_DIR)/SmartEnergy2/sep_2051.c -lexip -o $(EXAMPLES_BIN_DIR)/$(EXAMPLE_3)
