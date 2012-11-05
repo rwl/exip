eb57c89c325e71125c08beace12bc014644f109b
kjussakov <kjussakov@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2012-11-05 13:56:55 +0000

    refactoring examples

--- examples/simpleDecoding/decodeTestEXI.c
+++ examples/simpleDecoding/decodeTestEXI.c
@@ -16,18 +16,12 @@
  */
 #include "EXIParser.h"
 #include "stringManipulate.h"
-#include "grammarGenerator.h"
 #include <stdio.h>
 #include <string.h>
+#include "decodeTestEXI.h"
 
-static void printfHelp();
-static void parseSchema(char** fileNames, unsigned int schemaFilesCount, EXIPSchema* schema);
-
-#define OUT_EXI 0
-#define OUT_XML 1
 #define INPUT_BUFFER_SIZE 200
 #define MAX_PREFIXES 10
-#define MAX_XSD_FILES_COUNT 10 // up to 10 XSD files
 
 struct appData
 {
@@ -73,170 +67,70 @@ static char sample_booleanData(unsigned char bool_val, void* app_data);
 static char sample_dateTimeData(EXIPDateTime dt_val, void* app_data);
 static char sample_binaryData(const char* binary_val, Index nbytes, void* app_data);
 
-size_t readFileInputStream(void* buf, size_t readSize, void* stream);
-
-int main(int argc, char *argv[])
+errorCode decode(EXIPSchema* schemaPtr, unsigned char outFlag, FILE *infile, size_t (*inputStream)(void* buf, size_t size, void* stream))
 {
-	FILE *infile;
-	char sourceFileName[100];
-	EXIPSchema schema;
-	EXIPSchema* schemaPtr = NULL;
+	Parser testParser;
+	char buf[INPUT_BUFFER_SIZE];
+	BinaryBuffer buffer;
+	errorCode tmp_err_code = UNEXPECTED_ERROR;
 	struct appData parsingData;
-	unsigned int argIndex = 1;
 
-	parsingData.outputFormat = OUT_EXI; // Default output option
+	buffer.buf = buf;
+	buffer.bufLen = INPUT_BUFFER_SIZE;
+	buffer.bufContent = 0;
+	// Parsing steps:
 
-	if(argc > 1)
+	// I: First, define an external stream for the input to the parser if any
+	buffer.ioStrm.readWriteToStream = inputStream;
+	buffer.ioStrm.stream = infile;
+
+	// II: Second, initialize the parser object
+	tmp_err_code = initParser(&testParser, buffer, schemaPtr, &parsingData);
+	if(tmp_err_code != ERR_OK)
+		return tmp_err_code;
+
+	// III: Initialize the parsing data and hook the callback handlers to the parser object
+
+	parsingData.expectAttributeData = 0;
+	parsingData.stack = NULL;
+	parsingData.unclosedElement = 0;
+	parsingData.prefixesCount = 0;
+	parsingData.outputFormat = outFlag;
+
+	testParser.handler.fatalError = sample_fatalError;
+	testParser.handler.error = sample_fatalError;
+	testParser.handler.startDocument = sample_startDocument;
+	testParser.handler.endDocument = sample_endDocument;
+	testParser.handler.startElement = sample_startElement;
+	testParser.handler.attribute = sample_attribute;
+	testParser.handler.stringData = sample_stringData;
+	testParser.handler.endElement = sample_endElement;
+	testParser.handler.decimalData = sample_decimalData;
+	testParser.handler.intData = sample_intData;
+	testParser.handler.floatData = sample_floatData;
+	testParser.handler.booleanData = sample_booleanData;
+	testParser.handler.dateTimeData = sample_dateTimeData;
+	testParser.handler.binaryData = sample_binaryData;
+
+	// IV: Parse the header of the stream
+
+	tmp_err_code = parseHeader(&testParser);
+
+	// V: Parse the body of the EXI stream
+
+	while(tmp_err_code == ERR_OK)
 	{
-		if(strcmp(argv[argIndex], "-help") == 0)
-		{
-			printfHelp();
-			return 0;
-		}
-		else if(strcmp(argv[argIndex], "-exi") == 0)
-		{
-			parsingData.outputFormat = OUT_EXI;
-			if(argc == 2)
-			{
-				printfHelp();
-				return 0;
-			}
-
-			argIndex = 2;
-		}
-		else if(strcmp(argv[argIndex], "-xml") == 0)
-		{
-			parsingData.outputFormat = OUT_XML;
-			if(argc == 2)
-			{
-				printfHelp();
-				return 0;
-			}
-
-			argIndex = 2;
-		}
-
-		if(strcmp(argv[argIndex], "-schema") == 0)
-		{
-			if(argc <= argIndex + 2)
-			{
-				printfHelp();
-				return 0;
-			}
-			else
-			{
-				unsigned int schemaFilesCount = 0;
-
-				schemaFilesCount = argc - argIndex - 2;
-
-				argIndex++;
-
-				parseSchema(argv + argIndex, schemaFilesCount, &schema);
-				schemaPtr = &schema;
-
-				argIndex += schemaFilesCount;
-			}
-		}
-
-		strcpy(sourceFileName, argv[argIndex]);
-
-		infile = fopen(sourceFileName, "rb" );
-		if(!infile)
-		{
-			fprintf(stderr, "Unable to open file %s", sourceFileName);
-			return 1;
-		}
-		else
-		{
-			Parser testParser;
-			char buf[INPUT_BUFFER_SIZE];
-			BinaryBuffer buffer;
-			errorCode tmp_err_code = UNEXPECTED_ERROR;
-
-			buffer.buf = buf;
-			buffer.bufLen = INPUT_BUFFER_SIZE;
-			buffer.bufContent = 0;
-			// Parsing steps:
-
-			// I: First, define an external stream for the input to the parser if any
-			buffer.ioStrm.readWriteToStream = readFileInputStream;
-			buffer.ioStrm.stream = infile;
-
-			// II: Second, initialize the parser object
-			tmp_err_code = initParser(&testParser, buffer, schemaPtr, &parsingData);
-			if(tmp_err_code != ERR_OK)
-				return tmp_err_code;
-
-			// III: Initialize the parsing data and hook the callback handlers to the parser object
-
-			parsingData.expectAttributeData = 0;
-			parsingData.stack = NULL;
-			parsingData.unclosedElement = 0;
-			parsingData.prefixesCount = 0;
-
-			testParser.handler.fatalError = sample_fatalError;
-			testParser.handler.error = sample_fatalError;
-			testParser.handler.startDocument = sample_startDocument;
-			testParser.handler.endDocument = sample_endDocument;
-			testParser.handler.startElement = sample_startElement;
-			testParser.handler.attribute = sample_attribute;
-			testParser.handler.stringData = sample_stringData;
-			testParser.handler.endElement = sample_endElement;
-			testParser.handler.decimalData = sample_decimalData;
-			testParser.handler.intData = sample_intData;
-			testParser.handler.floatData = sample_floatData;
-			testParser.handler.booleanData = sample_booleanData;
-			testParser.handler.dateTimeData = sample_dateTimeData;
-			testParser.handler.binaryData = sample_binaryData;
-
-			// IV: Parse the header of the stream
-
-			tmp_err_code = parseHeader(&testParser);
-
-			// V: Parse the body of the EXI stream
-
-			while(tmp_err_code == ERR_OK)
-			{
-				tmp_err_code = parseNext(&testParser);
-			}
-
-			// VI: Free the memory allocated by the parser and schema object
-
-			destroyParser(&testParser);
-			if(schemaPtr != NULL)
-				destroySchema(schemaPtr);
-			fclose(infile);
-
-			if(tmp_err_code == PARSING_COMPLETE)
-				return ERR_OK;
-			else
-			{
-				printf("\nError during parsing of the EXI stream: %d", tmp_err_code);
-				return 1;
-			}
-		}
+		tmp_err_code = parseNext(&testParser);
 	}
+
+	// VI: Free the memory allocated by the parser
+
+	destroyParser(&testParser);
+
+	if(tmp_err_code == PARSING_COMPLETE)
+		return ERR_OK;
 	else
-	{
-		printfHelp();
-		return 1;
-	}
-	return 0;
-}
-
-static void printfHelp()
-{
-    printf("\n" );
-    printf("  EXIP     Copyright (c) 2010 - 2012, EISLAB - Luleå University of Technology Version 0.4 \n");
-    printf("           Authors: Rumen Kyusakov\n");
-    printf("  Usage:   exipd [options] <EXI_FileIn>\n\n");
-    printf("           Options: [-help | [ -xml | -exi ] -schema <schema_files_in>] \n");
-    printf("           -schema :   uses schema defined in <schema_files_in> for decoding\n");
-    printf("           -exi    :   EXI formated output [default]\n");
-    printf("           -xml    :   XML formated output\n");
-    printf("           -help   :   Prints this help message\n\n");
-    printf("  Purpose: This program tests the EXIP decoding functionality\n");
-    printf("\n" );
+		return tmp_err_code;
 }
 
 static char sample_fatalError(const char code, const char* msg, void* app_data)
@@ -740,72 +634,6 @@ static void destroyElement(struct element* el)
 	free(el);
 }
 // ******************************************
-
-size_t readFileInputStream(void* buf, size_t readSize, void* stream)
-{
-	FILE *infile = (FILE*) stream;
-	return fread(buf, 1, readSize, infile);
-}
-
-static void parseSchema(char** fileNames, unsigned int schemaFilesCount, EXIPSchema* schema)
-{
-	FILE *schemaFile;
-	BinaryBuffer buffer[MAX_XSD_FILES_COUNT];
-	errorCode tmp_err_code = UNEXPECTED_ERROR;
-	unsigned int i;
-
-	if(schemaFilesCount > MAX_XSD_FILES_COUNT)
-	{
-		fprintf(stderr, "Too many xsd files given as an input: %d", schemaFilesCount);
-		exit(1);
-	}
-
-	for(i = 0; i < schemaFilesCount; i++)
-	{
-		schemaFile = fopen(fileNames[i], "rb" );
-		if(!schemaFile)
-		{
-			fprintf(stderr, "Unable to open file %s", fileNames[i]);
-			exit(1);
-		}
-		else
-		{
-			//Get file length
-			fseek(schemaFile, 0, SEEK_END);
-			buffer[i].bufLen = ftell(schemaFile) + 1;
-			fseek(schemaFile, 0, SEEK_SET);
-
-			//Allocate memory
-			buffer[i].buf = (char *) malloc(buffer[i].bufLen);
-			if (!buffer[i].buf)
-			{
-				fprintf(stderr, "Memory allocation error!");
-				fclose(schemaFile);
-				exit(1);
-			}
-
-			//Read file contents into buffer
-			fread(buffer[i].buf, buffer[i].bufLen, 1, schemaFile);
-			fclose(schemaFile);
-
-			buffer[i].bufContent = buffer[i].bufLen;
-			buffer[i].ioStrm.readWriteToStream = NULL;
-			buffer[i].ioStrm.stream = NULL;
-		}
-	}
-
-	tmp_err_code = generateSchemaInformedGrammars(buffer, schemaFilesCount, SCHEMA_FORMAT_XSD_EXI, schema);
-	if(tmp_err_code != ERR_OK)
-	{
-		printf("\n Error occured: %d", tmp_err_code);
-		exit(1);
-	}
-
-	for(i = 0; i < schemaFilesCount; i++)
-	{
-		free(buffer[i].buf);
-	}
-}
 
 static char lookupPrefix(struct appData* aData, String ns, unsigned char* prxHit, unsigned char* prefixIndex)
 {
