7487342659e2f670962f2f6bcb6a39f8f5413b2e
kjussakov <kjussakov@3073ab62-7c58-44d6-84d6-47b9a845c6e8>
2013-02-05 14:08:40 +0000

    Refactoring of the Make build system: - remove multiple ,mk files - add Automatic Generating of Prerequisites (see http://www.gnu.org/software/make/manual/make.html#Automatic-Prerequisites)

--- build/gcc/grammar.mk
+++
@@ -1,6 +0,0 @@
-# StreamIO module build
-
-GRAMMAR_OBJ = $(BIN_DIR)/grammars.o
-
-$(BIN_DIR)/grammars.o : $(GRAMMAR_SRC)/include/grammars.h $(GRAMMAR_SRC)/src/grammars.c
-		$(COMPILE) -c $(GRAMMAR_SRC)/src/grammars.c -o $(BIN_DIR)/grammars.o
